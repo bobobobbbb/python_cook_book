ab445cd60f76b7fbed7ab8f36424652d4ea86395
bobobobbbb <1105388289@qq.com>
2017-04-15 01:03:31 +0800

    python cookbook read to node 4.8

---
+++ 4.8.py
@@ -0,0 +1,43 @@
+#Skipping the First Part of an Iterable
+>>> with open('/etc/passwd') as f:
+... for line in f:
+...     print(line, end='')
+...
+##
+# User Database
+#
+# Note that this file is consulted directly only when the system is running
+# in single-user mode. At other times, this information is provided by
+# Open Directory.
+...
+##
+nobody:*:-2:-2:Unprivileged User:/var/empty:/usr/bin/false
+root:*:0:0:System Administrator:/var/root:/bin/sh
+...
+>>>
+
+
+#If we want to skip all of the initial comment lines, here's one way to do it:
+>>> from itertools import dropwhile
+>>> with open('/etc/passwd') as f:
+...     for line in dropwhile(lambda line: line.startswith('#'), f):
+...         print(line, end='')
+...
+nobody:*:-2:-2:Unprivileged User:/var/empty:/usr/bin/false
+root:*:0:0:System Administrator:/var/root:/bin/sh
+...
+>>>
+
+#If you happen to know the exact number of items you want to skip
+>>> from itertools import islice
+>>> items = ['a', 'b', 'c', 1, 4, 10, 15]
+>>> for x in islice(items, 3, None):
+# also can exchange to
+# >>> for x in islice(iter(items), 3, None):
+...     print(x)
+...
+1
+4
+10
+15
+>>>
